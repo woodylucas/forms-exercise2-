b1d34791feb14592a2680abfc8563e21a00a7092
woodylucas <wlucas0311@gmail.com>
2020-11-14 09:36:50 -0500

    created form

--- src/NewBoxForm.js
+++ src/NewBoxForm.js
@@ -4,13 +4,46 @@ class NewBoxForm extends Component {
   constructor(props) {
     super(props);
     this.state = { height: "", width: "", color: "" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    });
   }
   render() {
     return (
       <form>
         <div>
-          <label>Height</label>
-          <input type="text" />
+          <label htmlFor="height">Height</label>
+          <input
+            type="text"
+            name="height"
+            value={this.state.height}
+            onChange={this.handleChange}
+            id="height"
+          />
+        </div>
+        <div>
+          <label htmlFor="width">Width</label>
+          <input
+            type="text"
+            name="width"
+            value={this.state.width}
+            onChange={this.handleChange}
+            id="width"
+          />
+        </div>
+        <div>
+          <label htmlFor="color">Color</label>
+          <input
+            type="text"
+            name="color"
+            value={this.state.color}
+            onChange={this.handleChange}
+            id="color"
+          />
         </div>
       </form>
     );
